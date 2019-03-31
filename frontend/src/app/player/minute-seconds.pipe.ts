9b65cbdfc96905e4aa031604739833d186e8342b
Tneciv <tnecivl@gmail.com>
2019-03-31 21:51:56 +0800

    fix player layout

--- frontend/src/app/player/minute-seconds.pipe.ts
+++ frontend/src/app/player/minute-seconds.pipe.ts
@@ -6,6 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MinuteSecondsPipe implements PipeTransform {
 
   transform(value: any): string {
+    if (!value) {
+      return;
+    }
     value = value.replace(new RegExp(',', 'g'), '');
     const minutes: number = Math.floor(value / 60);
     const seconds = value - minutes * 60 > 9 ? (value - minutes * 60) : '0' + (value - minutes * 60);
